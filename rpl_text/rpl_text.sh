22dcde144fae45de3cac0e8a77deffcd0323dd5b
yusoshi <nsymgnk0405@yahoo.co.jp>
2018-09-13 02:25:28 +0900

    特定のディレクトリ下のファイルの文字をすべて置換するシェル

---
+++ rpl_text/rpl_text.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# 置換したファイル群が存在するディレクトリを絶対パスで指定する
+dir=""
+
+# 置換したい文字を指定
+rpl=""
+
+# 置換後の文字を指定
+aftRlp=""
+
+# 特定のディレクトリ下のファイル内の特定の文字列を置換する 
+for filename in `ls ${dir}`
+do
+    sed -i '' s/$rpl/$aftRlp/g "${dir}/${filename}"
+done
