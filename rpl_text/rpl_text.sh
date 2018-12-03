808612cd8377b750729ef214922ef80ee880f997
Genki Nishiyama <nishiyama@Genki-MacBook-Air.local>
2018-12-03 18:17:53 +0900

    再帰的に書き換えるようにしました

--- rpl_text/rpl_text.sh
+++ rpl_text/rpl_text.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-# 置換したいファイル群が存在するディレクトリを絶対パスで指定する(最後のディレクトリに/は不要です。)
-# 例) dir="/User/testman/sample_dir"
-dir=""
+# 置換したいファイル群が存在するディレクトリを絶対パスで指定する(最後のディレク>トリに/は不要です。)
+# 例) dir="/User/saikouman/sample_dir"
+dir="/var/www/mt/htdocs"
 
 # 置換したい文字を指定
-rpl=""
+rpl="demo.solanowa.jp"
 
 # 置換後の文字を指定
-aftRlp=""
+aftRlp="demo7.solanowa.jp"
 
 # 特定のディレクトリ下のファイル内の特定の文字列を置換する 
-for filename in `ls ${dir}`
+# for filename in `ls ${dir}`
+for filename in `find "$dir" -type f `
 do
-    sed -i '' s/$rpl/$aftRlp/g "${dir}/${filename}"
+sed -i s/"$rpl"/"$aftRlp"/g "${filename}"
 done
